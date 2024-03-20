cf7f87ef0562fc29d1159a7df830b1c5fb8da347
Sorawit Suriyakarn <thepsint@gmail.com>
2024-03-20 22:38:42 +0700

    wip

---
+++ src/ZeroExTownStamp.sol
@@ -0,0 +1,71 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity 0.8.20;
+
+import {Ownable} from "solady/auth/Ownable.sol";
+import {ERC721} from "solady/tokens/ERC721.sol";
+import {Initializable} from "solady/utils/Initializable.sol";
+import {LibString} from "solady/utils/LibString.sol";
+
+contract ZeroExTownStamp is ERC721, Ownable, Initializable {
+    error MintNotAllowed();
+    error TransferNotAllowed();
+
+    bool public mintAllowed;
+    bool public transferAllowed;
+    uint public totalSupply;
+    string public baseURI;
+
+    mapping(uint => address) public visitors;
+    mapping(uint => uint) public timestamps;
+    mapping(address => bool) public visited;
+
+    function name() public pure override returns (string memory) {
+        return "0xTOWNSTAMP";
+    }
+
+    function symbol() public pure override returns (string memory) {
+        return "0xTOWNSTAMP";
+    }
+
+    function tokenURI(uint id) public view override returns (string memory) {
+        string memory uri = LibString.concat(baseURI, LibString.toString(id));
+        string memory visitor = LibString.concat("?visitor=", LibString.toHexString(visitors[id]));
+        string memory owner = LibString.concat("&owner=", LibString.toHexString(ownerOf(id)));
+        string memory ts = LibString.concat("&ts=", LibString.toString(timestamps[id]));
+        return string(abi.encodePacked(uri, visitor, owner, ts));
+    }
+
+    function initialize(string calldata _baseURI, address owner) external initializer {
+        baseURI = _baseURI;
+        _initializeOwner(owner);
+    }
+
+    function mint() external {
+        if (!mintAllowed || visited[msg.sender]) {
+            revert MintNotAllowed();
+        }
+        uint tokenId = ++totalSupply;
+        visitors[tokenId] = msg.sender;
+        timestamps[tokenId] = block.timestamp;
+        visited[msg.sender] = true;
+        _mint(msg.sender, tokenId);
+    }
+
+    function setMintAllowed(bool allowed) external onlyOwner {
+        mintAllowed = allowed;
+    }
+
+    function setTransferAllowed(bool allowed) external onlyOwner {
+        transferAllowed = allowed;
+    }
+
+    function setBaseURI(string calldata _baseURI) external onlyOwner {
+        baseURI = _baseURI;
+    }
+
+    function _beforeTokenTransfer(address from, address, uint) internal virtual override {
+        if (from != address(0) && !transferAllowed) {
+            revert TransferNotAllowed();
+        }
+    }
+}
---
+++ test/TestZeroExTownStamp.sol
@@ -0,0 +1,29 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity 0.8.20;
+
+import {Test} from "forge-std/Test.sol";
+import {console2} from "forge-std/console2.sol";
+
+import {ZeroExTownStamp} from "../src/ZeroExTownStamp.sol";
+
+contract TestZeroExTownStamp is Test {
+    function testMint() external {
+        ZeroExTownStamp town = new ZeroExTownStamp();
+        town.initialize("https://stamp.0x.town/", address(this));
+        town.setMintAllowed(true);
+        address u1 = vm.addr(1024);
+        vm.prank(u1);
+        vm.warp(1000000);
+        town.mint();
+        address u2 = vm.addr(1025);
+        vm.prank(u2);
+        vm.warp(2000000);
+        town.mint();
+        console2.log(town.tokenURI(1));
+        console2.log(town.tokenURI(2));
+
+        town.setTransferAllowed(true);
+        vm.prank(u2);
+        town.transferFrom(u2, address(this), 2);
+    }
+}

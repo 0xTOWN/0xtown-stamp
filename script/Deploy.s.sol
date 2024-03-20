c0fc59c48054faec448fd63ca897491863643847
Sorawit Suriyakarn <thepsint@gmail.com>
2024-03-20 22:52:37 +0700

    wip

--- script/Deploy.s.sol
+++ script/Deploy.s.sol
@@ -1,12 +1,24 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 
-import {Script, console} from "forge-std/Script.sol";
+import {Script} from "forge-std/Script.sol";
+import {TransparentUpgradeableProxy} from
+    "@openzeppelin/contracts/proxy/transparent/TransparentUpgradeableProxy.sol";
+import {ZeroExTownStamp} from "../src/ZeroExTownStamp.sol";
 
 contract Deploy is Script {
     function setUp() public {}
 
     function run() public {
-        vm.broadcast();
+        vm.startBroadcast();
+        ZeroExTownStamp impl = new ZeroExTownStamp();
+        impl.initialize("", address(0));
+        TransparentUpgradeableProxy proxy = new TransparentUpgradeableProxy(
+            address(impl),
+            msg.sender,
+            abi.encodeWithSelector(
+                ZeroExTownStamp.initialize.selector, "https://stamp.0x.town/", msg.sender
+            )
+        );
     }
 }
